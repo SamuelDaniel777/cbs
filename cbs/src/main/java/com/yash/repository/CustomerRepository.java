3e3b7feeb2cf415c96fde2c33f0d451a2175f8c6
akshaynanabhau.patil <akshaynanabhau.patil@YI1012532LT.Yash.com>
2021-10-18 23:51:30 +0530

    update

--- cbs/src/main/java/com/yash/repository/CustomerRepository.java
+++ cbs/src/main/java/com/yash/repository/CustomerRepository.java
@@ -2,13 +2,14 @@ package com.yash.repository;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 import com.yash.domain.Customer;
 
 @Repository
-public interface CustomerRepository extends CrudRepository<Customer,Integer> 
+public interface CustomerRepository extends JpaRepository<Customer,Integer> 
 {
 	List<Customer> findById(int id);
 }
