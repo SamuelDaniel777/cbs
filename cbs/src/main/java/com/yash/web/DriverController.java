6bbaed485d1c2188cf95daef4c0c9173950a3188
anuj.agarwal <anuj.agarwal@YI1010525LT.Yash.com>
2021-10-20 12:33:13 +0530

    Driver Controller added

--- cbs/src/main/java/com/yash/web/DriverController.java
+++ cbs/src/main/java/com/yash/web/DriverController.java
@@ -1,24 +1,40 @@
 package com.yash.web;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yash.domain.Driver;
 import com.yash.repository.DriverRepository;
+import com.yash.serviceimpl.DriverServiceImpl;
 
 @RestController
+@RequestMapping("/driver")
 public class DriverController {
 
 	@Autowired
-	DriverRepository driverRepository;
+	DriverServiceImpl driverServiceImpl;
 	
-	@PostMapping("/driverSave")
-	public String insertDriver(@RequestBody Driver driver)
+	@PostMapping("/insertDriver")
+	public String insertDriver(Driver driver)
 	{
-		driverRepository.save(driver);
+		driverServiceImpl.registerDriver(driver);
 		return "Your record is saved successfully";
 	}
+	
+	
+	
+	
+//	@GetMapping("/viewDriver/{driverId}")
+//	public Optional<Driver> viewDriver(@PathVariable("driverId") int id)
+//	{
+//		return driverRepository.findById(id);
+//	}
 	
 }
